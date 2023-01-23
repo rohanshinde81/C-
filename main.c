d6e47c74db8843e1e6f5abfb4b8723704b0e7035
Rohan Shinde <120273586+rohanshinde81@users.noreply.github.com>
2023-01-23 18:35:21 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+ 
+ 
+#include <stdio.h>
+
+int main()
+{
+    
+int a,b,c;
+printf("Enter the first Number");
+scanf("%d",&a);
+printf("Enter the second Number");
+scanf("%d",&b);
+c=a+b;
+printf("The addition of %d and %d = %d",a,b,c);
+    return 0;
+}
+
+

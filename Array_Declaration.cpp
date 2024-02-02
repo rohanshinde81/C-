877aaaa62802f3fde386cc56d284ff069a81217a
Rohan Shinde <120273586+rohanshinde81@users.noreply.github.com>
2024-02-02 18:05:45 +0530

    Create Array_Declaration.cpp

---
+++ Array_Declaration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[7];
+    cout<<arr<<endl;
+    cout<<&arr<<endl;  //& is used for the finding address of the number
+    cout<<"Array Created successfully"<<endl;
+
+    return 0;
+}

eeab06691017126553e06db0eb3ed9c78a2ab9d3
Rohan Shinde <120273586+rohanshinde81@users.noreply.github.com>
2024-02-02 18:07:05 +0530

    Create Array_Initialization.cpp

---
+++ Array_Initialization.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {2,3,5,6,7};
+    int brr[7] = {2,4,5,6,7,8,9};
+    int crr[10] = {3,5,7,9,2,5};
+    cout<<"Array Initialise Successfully"<<endl;
+    
+    //error
+ //   int drr[4] = {4,5,6,7,8,8};      //too many initializers for ‘int [4]’
+
+    return 0;
+}

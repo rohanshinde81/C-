46f93c2c07ef8defe405676e08c037f5388e6c6b
Rohan Shinde <120273586+rohanshinde81@users.noreply.github.com>
2024-02-03 10:14:21 +0530

    Create Volume_of_Cube.cpp

---
+++ Volume_of_Cube.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+  int sid1;
+  float volcu;
+  cout << " enter the one side of a cube :\n ";
+  cin >> sid1;
+  volcu = (sid1 * sid1 * sid1);
+  cout << " The volume of a cube is :\n " << volcu << endl;
+  cout << endl;
+  return 0;
+}

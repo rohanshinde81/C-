472f91a54c79127f078156319ff239f2d826114d
Rohan Shinde <120273586+rohanshinde81@users.noreply.github.com>
2024-02-10 08:42:16 +0530

    Create Area_of_Square.cpp

---
+++ Area_of_Square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int area, r;
+    cout<<"Enetr your side value :";
+    cin>>r;
+    area = r * r;
+    cout<<"Area of square is :"<<area;
+}
